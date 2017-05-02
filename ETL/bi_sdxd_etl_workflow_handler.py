54771397a42150e2c646f4ae4bfefa2ddbf248f5
robin_xiong <xiongzhibin1987@126.com>
2017-05-03 03:33:56 +0800

    initial

--- ETL/bi_sdxd_etl_workflow_handler.py
+++ ETL/bi_sdxd_etl_workflow_handler.py
@@ -181,7 +181,7 @@ if __name__ == "__main__":
     global today_str
     today_str = today.strftime('%Y-%m-%d')
     global curr_dir
-    curr_dir = os.getcwd()
+    curr_dir = os.path.dirname(__name__)
 
     global messager
     messager = {'table_name': '', 'action':'','data_hub':'',  'data_file': '', 'ctrl_file': '', 'ctrl_count': '', 'data_path_file': '', 'ctrl_path_file': '', 'status': 'Start', 'from_date': '9999-12-31', 'to_date': '9999-12-31','log':'log'}
@@ -189,9 +189,9 @@ if __name__ == "__main__":
     print '-INFO: parsing arguments'
     global runDate, runMode, actionType, tblName
     parser = argparse.ArgumentParser(description='Process')
-    parser.add_argument('-d', help='date for running with format "yyyymmdd"')
-    parser.add_argument('-m', help='date for running with format "yyyymmdd"')
-    parser.add_argument('-a', required=True, help='action of process, extract(e) or load(l)')
+    parser.add_argument('-d', help='date for running with format "yyyymmdd" e.g 20170101')
+    parser.add_argument('-m', help="date for running with format 't+n' e.g t+3")
+    parser.add_argument('-a', required=True, help="action of process, support option: 'extract'or 'load'")
     # parser.add_argument('-o', required=True, nargs='+', help='table object name you want to extract or load')
     parser.add_argument('-t', required=True, help='table object name you want to extract or load')
     args = parser.parse_args()
@@ -199,6 +199,8 @@ if __name__ == "__main__":
     runMode = str(args.m)
     actionType = str(args.a)
     tblName = str(args.t)
+
+    parser.print_help()
 
     global logger
     logger, logger_file = get_logger(curr_dir, tblName, runDate, start_time)
